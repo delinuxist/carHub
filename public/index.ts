d3cceda178e3fb84b90a4a5309c00993b66dab2f
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-26 21:37:15 +0000

    assets and global styles

---
+++ public/index.ts
@@ -0,0 +1,7 @@
+import carLogo from "./car-logo.svg";
+import steeringWheel from "./steering-wheel.svg";
+import tire from "./tire.svg";
+import gas from "./gas.svg";
+import rightArrow from "./right-arrow.svg";
+
+export { carLogo, steeringWheel, tire, gas, rightArrow };
