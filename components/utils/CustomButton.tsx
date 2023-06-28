28b3bb029dcf0944629532189136c52fe9b1fbad
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-28 23:49:17 +0000

    custom button update

---
+++ components/utils/CustomButton.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Image from 'next/image'
+import { CustomButtonProps } from '../../shared/interfaces'
+
+const CustomButton = (Props: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={Props.buttonType}
+      className={`custom-btn ${Props.containerStyles} `}
+      onClick={Props.handleClick}
+    >
+      <span className={`flex-1 ${Props.textStyles}`}>
+        {Props.title}
+      </span>
+      {Props.rightIcon && (<div className='relative w-6 h-6'>
+        <Image src={Props.rightIcon} alt='right-icon' />
+      </div>)}
+    </button>
+  )
+}
+
+export default CustomButton
